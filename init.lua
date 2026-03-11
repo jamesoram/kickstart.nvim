d29a8baae7f94d14da8a2a0f8289bab9de20e6de
James Oram <herespam1@hotmail.com>
2026-03-11 12:01:36 +0000

    add language servers for python and rust

--- init.lua
+++ init.lua
@@ -622,8 +622,8 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         ruby_lsp = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
